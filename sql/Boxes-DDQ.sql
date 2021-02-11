fd042e72bb663f9c954b1834ac40378bdf720225
Amelia Walsh <ameliatwalsh@gmail.com>
2021-02-11 14:06:38 -0800

    adds DDQ for Boxes, Customer, and Boxes_Customer tables

--- sql/Boxes-DDQ.sql
+++ sql/Boxes-DDQ.sql
@@ -4,8 +4,10 @@
 --
     -- box_id: INT, PK, auto_increment
     -- box_date: DATE, not NULL
---
---
---
 
+CREATE TABLE Boxes(
+    box_id INT NOT NULL AUTO_INCREMENT,
+    box_date DATE NOT NULL,
+    PRIMARY KEY (box_id)
+);
 
