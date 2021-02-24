8eade9671cd76a9fb2c1f2581cb1f8a41c4aca2d
Amelia Walsh <ameliatwalsh@gmail.com>
2021-02-23 18:28:34 -0800

    spacing change

--- resources/js/adminHandler.js
+++ resources/js/adminHandler.js
@@ -1,10 +1,52 @@
 // name: adminHandler.js, creator: Amelia Walsh, date: 1/31/21
 // handles buttons within the admin handlebars pages
 
+var express = require('express');
+var mysql = require('./dbcon.js');
+var CORS = require('cors');
+
+var app = express();
+app.set('port', 28394);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(CORS());
+
+// ///////////////////////
+//      ADMIN PAGES
+// ///////////////////////
+
+// ***********************
+// ***** New Customer ****
+// ***********************
+
 const get_all_customers = 'SELECT * FROM Customers';
 const insert_customers = "INSERT INTO Customers (first_name, last_name, date_paid) VALUES(?,?,?);"
 
+const getAllData = (res) => {
+    var context = {};
+    mysql.pool.query(get_all_customers, (err, rows, fields) => {
+      if(err){
+        return;
+      }
+      res.json({"rows": rows});
+    });
+  };
 
+
+app.post('/',function(req,res,next){
+    // var context = {};
+    var {first_name, last_name, date_paid} = req.body;
+    mysql.pool.query(insert_customers, [first_name, last_name, date_paid], (err, result)=>{
+      if(err){
+        next(err);
+        return;
+      }
+      
+      getAllData(res);
+  
+    });
+  });
 
 
 
